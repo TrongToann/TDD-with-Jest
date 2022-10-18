4c89addf8c3233c5a25cd8a61a9df54435137726
TrongToann <nguyentrongtoan9a1nbk@gmail.com>
2022-10-19 00:21:47 +0700

    update Jest final

--- js/pascal.test.js
+++ js/pascal.test.js
@@ -2,6 +2,3 @@ const arrayPascal = require('./pascal')
 test('test arrayPascal', function() {
     expect(arrayPascal(5)).toEqual([1, 5, 10, 10, 5, 1]) ;
 })
-test('test arrayPascal', function() {
-    expect(arrayPascal(4)).toEqual([1, 5, 10, 10, 5, 1]) ;
-})
