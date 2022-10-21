1bd8206feaf8ca4e3c010b952d8bad695765c8a6
TrongToann <nguyentrongtoan9a1nbk@gmail.com>
2022-10-21 21:53:58 +0700

    final update

--- js/pascal.test.js
+++ js/pascal.test.js
@@ -2,7 +2,7 @@ const arrayPascal = require('./pascal')
 
 describe("Test ArrayPascal", () => {
     it("Test Function with right ", () => {
-        expect(arrayPascal(4)).toEqual([1, 5, 10, 10, 5, 1]) ;
+        expect(arrayPascal(5)).toEqual([1, 5, 10, 10, 5, 1]) ;
     })
     it("Test Function with wrong", () => {
         expect(() => {arrayPascal(6) } ).toThrowError("Invalid Number!") ; 
